66c5c1c2ba2291089dc6a178bae5fd6b4b4863b9
Kliukhin Konstantin <kliukhinkonstantin@gmail.com>
2024-01-11 17:47:42 +0200

    fixed layers rule

--- tests/lib/rules/layer-imports.js
+++ tests/lib/rules/layer-imports.js
@@ -18,43 +18,43 @@ const ruleTester = new RuleTester({
 ruleTester.run("layer-imports", rule, {
   valid: [
     {
-      filename: 'C:\\Users\\tim\\Desktop\\javascript\\production_project\\src\\features\\Article',
+      filename: 'C:/Users/tim/Desktop/javascript/production_project/src/features/Article',
       code: "import { addCommentFormActions, addCommentFormReducer } from '@/shared/Button.tsx'",
       errors: [],
       options: aliasOptions,
     },
     {
-      filename: 'C:\\Users\\tim\\Desktop\\javascript\\production_project\\src\\features\\Article',
+      filename: 'C:/Users/tim/Desktop/javascript/production_project/src/features/Article',
       code: "import { addCommentFormActions, addCommentFormReducer } from '@/entities/Article'",
       errors: [],
       options: aliasOptions,
     },
     {
-      filename: 'C:\\Users\\tim\\Desktop\\javascript\\production_project\\src\\app\\providers',
+      filename: 'C:/Users/tim/Desktop/javascript/production_project/src/app/providers',
       code: "import { addCommentFormActions, addCommentFormReducer } from '@/widgets/Articl'",
       errors: [],
       options: aliasOptions,
     },
     {
-      filename: 'C:\\Users\\tim\\Desktop\\javascript\\production_project\\src\\widgets\\pages',
+      filename: 'C:/Users/tim/Desktop/javascript/production_project/src/widgets/pages',
       code: "import { useLocation } from 'react-router-dom'",
       errors: [],
       options: aliasOptions,
     },
     {
-      filename: 'C:\\Users\\tim\\Desktop\\javascript\\production_project\\src\\app\\providers',
+      filename: 'C:/Users/tim/Desktop/javascript/production_project/src/app/providers',
       code: "import { addCommentFormActions, addCommentFormReducer } from 'redux'",
       errors: [],
       options: aliasOptions,
     },
     {
-      filename: 'C:\\Users\\tim\\Desktop\\javascript\\production_project\\src\\index.tsx',
+      filename: 'C:/Users/tim/Desktop/javascript/production_project/src/index.tsx',
       code: "import { StoreProvider } from '@/app/providers/StoreProvider';",
       errors: [],
       options: aliasOptions,
     },
     {
-      filename: 'C:\\Users\\tim\\Desktop\\javascript\\production_project\\src\\entities\\Article.tsx',
+      filename: 'C:/Users/tim/Desktop/javascript/production_project/src/entities/Article.tsx',
       code: "import { StateSchema } from '@/app/providers/StoreProvider'",
       errors: [],
       options: [
@@ -68,19 +68,19 @@ ruleTester.run("layer-imports", rule, {
 
   invalid: [
     {
-      filename: 'C:\\Users\\tim\\Desktop\\javascript\\production_project\\src\\entities\\providers',
+      filename: 'C:/Users/tim/Desktop/javascript/production_project/src/entities/providers',
       code: "import { addCommentFormActions, addCommentFormReducer } from '@/features/Articl'",
       errors: [{ messageId: "incorrect-layer-import"}],
       options: aliasOptions,
     },
     {
-      filename: 'C:\\Users\\tim\\Desktop\\javascript\\production_project\\src\\features\\providers',
+      filename: 'C:/Users/tim/Desktop/javascript/production_project/src/features/providers',
       code: "import { addCommentFormActions, addCommentFormReducer } from '@/widgets/Articl'",
       errors: [{ messageId: "incorrect-layer-import"}],
       options: aliasOptions,
     },
     {
-      filename: 'C:\\Users\\tim\\Desktop\\javascript\\production_project\\src\\entities\\providers',
+      filename: 'C:/Users/tim/Desktop/javascript/production_project/src/entities/providers',
       code: "import { addCommentFormActions, addCommentFormReducer } from '@/widgets/Articl'",
       errors: [{ messageId: "incorrect-layer-import"}],
       options: aliasOptions,
