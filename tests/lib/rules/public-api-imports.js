96d1439b090beb31d0ebcf683410c66d5ffb4146
Kliukhin Konstantin <kliukhinkonstantin@gmail.com>
2024-01-11 14:55:13 +0200

    added public-api-imports rule

---
+++ tests/lib/rules/public-api-imports.js
@@ -0,0 +1,55 @@
+/**
+ * @fileoverview restricts imports not from public api
+ * @author Kliukhin Konstantin
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const rule = require("../../../lib/rules/public-api-imports"),
+  RuleTester = require("eslint").RuleTester;
+
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester({
+  parserOptions: { ecmaVersion: 6, sourceType: 'module'}
+});
+
+ruleTester.run("public-api-imports", rule, {
+  valid: [
+    {
+      code: "import { Some } from 'entities/Some'",
+      errors: [],
+    },
+    {
+      code: "import { Some } from 'react-redux/some/deep/module'",
+      errors: [],
+    },
+    {
+      code: "import { Some } from '../../../../entities/Some/model/some.js'",
+      errors: [],
+    },
+  ],
+  invalid: [
+    {
+      code: "import { Some } from 'entities/Some/model/some.js'",
+      errors: [{ message: "You can import only from public api", type: "ImportDeclaration" }],
+    },
+    {
+      code: "import { Some } from 'features/Some/model/some.js'",
+      errors: [{ message: "You can import only from public api", type: "ImportDeclaration" }],
+    },
+    {
+      code: "import { Some } from '@/widgets/Some/model/some.js'",
+      options: [{
+        alias: '@'
+      }],
+      errors: [{ message: "You can import only from public api", type: "ImportDeclaration" }],
+    },
+  ],
+});
