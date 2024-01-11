10b6debb0ecf199b57c74f4430932588a84ef345
Kliukhin Konstantin <kliukhinkonstantin@gmail.com>
2024-01-11 14:20:40 +0200

    added ability to add alias option

--- tests/lib/rules/path-checker.js
+++ tests/lib/rules/path-checker.js
@@ -16,20 +16,37 @@ const rule = require("../../../lib/rules/path-checker"),
 // Tests
 //------------------------------------------------------------------------------
 
-const ruleTester = new RuleTester();
+const ruleTester = new RuleTester({
+  parserOptions: { ecmaVersion: 6, sourceType: 'module'}
+});
 ruleTester.run("path-checker", rule, {
   valid: [
-    // give me some code that won't trigger a warning
+    {
+      filename: '/src/entities/Article/some.js',
+      code: "import { Button } from 'shared/ui'",
+      errors: [],
+    },
+    {
+      filename: '/src/entities/Article/some.js',
+      code: "import { Article } from './model.js'",
+      errors: [],
+    },
   ],
 
   invalid: [
     {
       filename: '/src/entities/Article/some.js',
-      "parserOptions": {
-        "sourceType": "module",
-        ecmaVersion: 2015
-      },
       code: "import { article } from 'entities/Article'",
+      errors: [{ message: "All imports should be relative in one slice", type: "ImportDeclaration" }],
+    },
+    {
+      filename: '/src/entities/Article/some.js',
+      options: [
+        {
+          alias: "@"
+        }
+      ],
+      code: "import { article } from '@/entities/Article'",
       errors: [{ message: "All imports should be relative in one slice", type: "ImportDeclaration" }],
     },
   ],
